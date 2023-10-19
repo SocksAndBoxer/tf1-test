2beb0366df983697f0159364478f5da72f35d05f
Guillaume Marc <marcguillaume77@gmail.com>
2023-10-19 17:08:36 +0200

    Add global reset css file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TProgam } from './types/program'
 import QUERY_PROGRAMS from './graphql/program'
 import { useState } from 'react'
 import Slider from './components/Slider/Slider'
+import { styled } from 'styled-components'
 
 function App() {
   const [initialVariables, setInitialVariables] = useState({
@@ -20,21 +21,32 @@ function App() {
   console.log('1', data, error, loading)
 
   return (
-    <Slider itemLength={6}>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-    </Slider>
+    <SliderContainer>
+      <Slider itemWidth={200} itemLength={6}>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </Slider>
+    </SliderContainer>
   )
 }
+
+const SliderContainer = styled.div`
+  color: white;
+  height: 869px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ApolloProvider } from '@apollo/client/react'
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import GlobalStyle from './globalStyles.ts'
 
 const client = new ApolloClient({
   uri: 'https://tf1-interview.hasura.app/v1/graphql',
@@ -12,6 +13,7 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
+      <GlobalStyle />
       <App />
     </ApolloProvider>
   </React.StrictMode>
